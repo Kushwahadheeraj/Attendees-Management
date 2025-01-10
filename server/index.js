039efcaefa83b92dcf2b6572aaa9fee34eb71fea
Dheeraj Kushwaha <79093514+Kushwahadheeraj@users.noreply.github.com>
2025-01-10 11:45:04 +0530

    forth

--- server/index.js
+++ server/index.js
@@ -1,10 +1,31 @@
-import express from 'express';
-import cors from 'cors';
-
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+// const bodyParser = require("body-parser")
 const app = express()
-app.use(cors());
-app.use(express.json())
+const Routes = require("./routes/route.js")
 
-app.listen(process.env.PORT, () =>{
-    console.log(`Server is Running on port ${process.env.PORT}`)
+const PORT = process.env.PORT || 5000
+
+dotenv.config();
+
+// app.use(bodyParser.json({ limit: '10mb', extended: true }))
+// app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
+
+app.use(express.json({ limit: '10mb' }))
+app.use(cors())
+
+mongoose
+    .connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(console.log("Connected to MongoDB"))
+    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+
+app.use('/', Routes);
+
+app.listen(PORT, () => {
+    console.log(`Server started at port no. ${PORT}`)
 })
